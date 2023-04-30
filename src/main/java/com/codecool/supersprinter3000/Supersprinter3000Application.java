11a2d56acf8db60ab94617928b2737f0adc89af6
ColJops <jops24@o2.pl>
2023-04-30 21:37:56 +0200

    Start project

---
+++ src/main/java/com/codecool/supersprinter3000/Supersprinter3000Application.java
@@ -0,0 +1,13 @@
+package com.codecool.supersprinter3000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Supersprinter3000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Supersprinter3000Application.class, args);
+    }
+
+}
