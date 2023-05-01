abb3a5885a82a713cea7efde99188f28d7a31e8d
ColJops <94141667+ColJops@users.noreply.github.com>
2023-05-01 18:12:31 +0200

    Add files via upload
    Fix repos - Update, add new story + templates

--- src/main/java/com/codecool/supersprinter3000/controller/UserStoryController.java
+++ src/main/java/com/codecool/supersprinter3000/controller/UserStoryController.java
@@ -2,32 +2,64 @@ package com.codecool.supersprinter3000.controller;
 
 import com.codecool.supersprinter3000.entity.UserStory;
 import com.codecool.supersprinter3000.entity.UserStoryStatus;
+import com.codecool.supersprinter3000.service.UserStoryService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class UserStoryController {
 
+    private final UserStoryService userStoryService;
+
+//    @Autowired
+    public UserStoryController(UserStoryService userStoryService) {
+        this.userStoryService = userStoryService;
+    }
+
     @GetMapping("/")
-    public String allUsersStories(Model model) {
-        List<UserStory> userStories = List.of(new UserStory(1L,
-                "User Story Title",
-                "User Story",
-                "Must be done",
-                0.5,
-                100,
-                UserStoryStatus.TODO),
-                new UserStory(2L,
-                        "User Story Title1",
-                        "User Story1",
-                        "Must be done",
-                        0.7,
-                        120,
-                        UserStoryStatus.TODO));
-        model.addAttribute("userStories", userStories);
+    public String allUserStories(Model model) {
+        model.addAttribute("userStories", userStoryService.getAllUserStories());
         return "index";
+    }
+
+    @GetMapping("/new-user-story")
+    public String newUserStoryPage(UserStory userStory) {
+        return "new_user_story";
+    }
+
+    @GetMapping("/story/{id}")
+    public String userStoryPage(@PathVariable Long id, Model model) {
+        UserStory userStory = userStoryService.getUserStory(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Post not found"));
+
+        model.addAttribute("userStory", userStory);
+        return "new_user_story";
+    }
+
+    @PostMapping("/new-user-story")
+    public String createOrUpdateUserStory(@Valid UserStory userStory, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "new_user_story";
+        }
+        userStoryService.createOrUpdateUserStory(userStory);
+        return "redirect:/";
+    }
+
+    @ModelAttribute("dropDownAllStatuses")
+    private List<String> getUserStoriesStatuses() {
+        return Arrays.stream(UserStoryStatus.values())
+                .map(Enum::name)
+                .toList();
     }
 }
